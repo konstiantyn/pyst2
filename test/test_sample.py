83a18cc7615a6c94d5b268a905d397611d0c1920
poing <brian.lavallee@invite-comm.jp>
2020-10-11 07:57:43 +0900

    base travis

--- test/test_sample.py
+++ test/test_sample.py
@@ -6,3 +6,17 @@ def inc(x):
     
 def test_ok():
     assert 5 == 5
+    
+    
+import subprocess, platform
+def pingOk(sHost):
+    try:
+        output = subprocess.check_output("ping -{} 1 {}".format('n' if platform.system().lower()=="windows" else 'c', sHost), shell=True)
+
+    except Exception, e:
+        return False
+
+    return True
+
+def test_ping_google():
+	assert pingOk('8.8.8.8')
