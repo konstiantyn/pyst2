e3b2f6058380842d822b88731772225e9e951745
poing <brian.lavallee@invite-comm.jp>
2020-10-11 08:59:35 +0900

    base travis

---
+++ test/test_manager_connect.py
@@ -0,0 +1,8 @@
+import asterisk.manager
+
+manager = asterisk.manager.Manager()
+try:
+  manager.connect("172.25.0.101")
+  manager.login('pyst2', '5kcn4MCVTk6qCrdq')
+  
+  assert manager.connected()
