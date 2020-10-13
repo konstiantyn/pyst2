7bf221fd1da683e1df0ddd772deb4d240547757c
poing <brian.lavallee@invite-comm.jp>
2020-10-13 18:49:45 +0900

    fix user on asterisk, i hope

--- test/docker-entrypoint.sh
+++ test/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # run as user asterisk by default
-ASTERISK_USER=${ASTERISK_USER:-asterisk}
-ASTERISK_GROUP=${ASTERISK_GROUP:-${ASTERISK_USER}}
+ASTERISK_USER=travis
+ASTERISK_GROUP=travis
 
 if [ "$1" = "" ]; then
   COMMAND="/usr/sbin/asterisk -T -W -U ${ASTERISK_USER} -p -vvvdddf"
@@ -14,9 +14,9 @@ if [ "${ASTERISK_UID}" != "" ] && [ "${ASTERISK_GID}" != "" ]; then
   # recreate user and group for asterisk
   # if they've sent as env variables (i.e. to macth with host user to fix permissions for mounted folders
 
-  deluser asterisk && \
-  addgroup -g ${ASTERISK_GID} ${ASTERISK_GROUP} && \
-  adduser -D -H -u ${ASTERISK_UID} -G ${ASTERISK_GROUP} ${ASTERISK_USER} \
+  deluser travis && \
+  addgroup -g 2000 travis && \
+  adduser -D -H -u 2000 -G travis travis \
   || exit
 fi
 
