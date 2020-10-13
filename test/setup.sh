4692c25022979f6d9773aab02e32dfb0ce993f3a
poing <brian.lavallee@invite-comm.jp>
2020-10-14 00:11:06 +0900

    coverage VIII

--- test/setup.sh
+++ test/setup.sh
@@ -2,5 +2,5 @@
 pip install /var/lib/asterisk/agi-bin coverage
 cd /var/lib/asterisk/agi-bin/
 chmod a+rw .coverage
-chown travis:travis .coverage
+chown asterisk:asterisk .coverage
 coverage run test/other.py
