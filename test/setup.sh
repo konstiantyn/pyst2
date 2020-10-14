5575d5aef3466e8740f420f432c06e503d4f3df7
poing <brian.lavallee@invite-comm.jp>
2020-10-14 18:00:32 +0900

    seperate cov files XX

--- test/setup.sh
+++ test/setup.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 pip install ${TRAVIS_BUILD_DIR} coverage
 cd ${TRAVIS_BUILD_DIR}
-#chmod a+rw .coverage
-#chown asterisk:asterisk .coverage
 coverage run --rcfile=.coveragerc.asterisk test/other.py
-#coverage run --source ./ -m pytest
