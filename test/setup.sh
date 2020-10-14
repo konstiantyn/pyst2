35a8f681a329037247a87c9c8a3ea19943229423
poing <brian.lavallee@invite-comm.jp>
2020-10-14 16:12:02 +0900

    seperate cov files XIV

--- test/setup.sh
+++ test/setup.sh
@@ -3,5 +3,5 @@ pip install /home/travis/build/invitecomm/pyst2 coverage coveralls pytest
 cd /home/travis/build/invitecomm/pyst2
 #chmod a+rw .coverage
 #chown asterisk:asterisk .coverage
-#coverage run --source ./ test/other.py
-coverage run --source ./ -m pytest
+coverage run --source ./ test/other.py
+#coverage run --source ./ -m pytest
