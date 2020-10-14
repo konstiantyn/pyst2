52447e32ec52d966574c2f6f3fbf55d8a5258577
poing <brian.lavallee@invite-comm.jp>
2020-10-14 14:32:40 +0900

    seperate cov files VIII

--- test/setup.sh
+++ test/setup.sh
@@ -3,4 +3,4 @@ pip install /var/lib/asterisk/agi-bin coverage
 cd /var/lib/asterisk/agi-bin/
 #chmod a+rw .coverage
 #chown asterisk:asterisk .coverage
-coverage run -a test/other.py
+coverage run test/other.py
